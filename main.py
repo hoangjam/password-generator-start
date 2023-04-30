dd7a1044d805bd3ef98d900464d9b205ca6cc94c
hoangjam <36323359+hoangjam@users.noreply.github.com>
2023-04-30 01:20:20 +0000

    completed randomized hard password.

--- main.py
+++ main.py
@@ -11,21 +11,35 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 #Eazy Level - Order not randomised:
 #e.g. 4 letter, 2 symbol, 2 number = JduE&!91
-easyPassword = ""
-for l in range(0, nr_letters):
-  easyPassword += letters[random.randint(0, len(letters) -1)]
 
-for s in range(0, nr_symbols):
-  easyPassword += numbers[random.randint(0, len(numbers) -1)]
+# easyPassword = ""
+# for l in range(1, nr_letters + 1):
+#   easyPassword += letters[random.randint(0, len(letters) -1)]
 
-for n in range(0, nr_numbers):
-  easyPassword += symbols[random.randint(0, len(symbols) -1)]
+# for s in range(1, nr_symbols + 1):
+#   easyPassword += numbers[random.randint(0, len(numbers) -1)]
 
-print(f"Your easy password is: {easyPassword}")
+# for n in range(1, nr_numbers + 1):
+#   easyPassword += symbols[random.randint(0, len(symbols) -1)]
+
+# print(f"Your easy password is: {easyPassword}")
 
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+hardPasswordList = []
 hardPassword = ""
 
+for l in range(1, nr_letters + 1):
+  hardPasswordList.append(letters[random.randint(0, len(letters) -1)])
+
+for s in range(1, nr_symbols + 1):
+  hardPasswordList.append(numbers[random.randint(0, len(numbers) -1)])
+
+for n in range(1, nr_numbers + 1):
+  hardPasswordList.append(symbols[random.randint(0, len(symbols) -1)])
+
+random.shuffle(hardPasswordList)
+for c in hardPasswordList:
+  hardPassword += c
 
 print(f"Your hard password is: {hardPassword}")
